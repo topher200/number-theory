3007cb80425f4b45974a47b92f358570d0af3886
Topher Brown <topher200@gmail.com>
2011-05-05 22:37:01 -0400

    number_theory: playing many times

--- number_theory.py
+++ number_theory.py
@@ -8,13 +8,25 @@ def choose_randomly():
 def roll_die():
   return choose_randomly()
 
-def play_randomly():
+def play(guess_function):
   roll1 = roll_die()
   roll2 = roll_die()
 
-  guess1 = choose_randomly()
-  guess2 = choose_randomly()
+  guess1 = guess_function()
+  guess2 = guess_function()
 
   return ((roll1 == guess1) and (roll2 == guess2))
 
-print(play_randomly())
+wins = 0
+losses = 0
+for i in range(1000000):
+  # Play, and record the outcome
+  if play(choose_randomly):
+    wins += 1
+  else:
+    losses += 1
+
+total_games = wins + losses
+win_percentage = wins / total_games
+print(wins, losses, win_percentage)
+  
